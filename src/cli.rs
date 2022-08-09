326135b71c37f357f4aa7209841a28f47d5747c8
ERUIHNIYHBKBNF <839203521@qq.com>
2022-08-09 18:33:09 +0800

    feat: cli

---
+++ src/cli.rs
@@ -0,0 +1,43 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+pub enum Action {
+    /// Load a module
+    Load {
+        #[structopt()]
+        path: String,
+    },
+    /// Unload a module
+    Unload {
+        #[structopt()]
+        module_name: String,
+    },
+    /// List loaded modules
+    List,
+    /// List functions in a module
+    Listfn {
+        #[structopt()]
+        module_name: String,
+    },
+    /// Call a function in a module
+    Call {
+        #[structopt()]
+        module_name: String,
+        #[structopt()]
+        function_name: String,
+        #[structopt()]
+        args: Vec<String>,
+    },
+    /// Exits the program
+    Exit,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "terminal-hostcall",
+    about = "A simple CLI for interacting with the hostcall module"
+)]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+mod cli;
+use cli::{Action::*, CommandLineArgs};
+use std::io;
+use structopt::StructOpt;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        let mut args: Vec<String> = line.split_whitespace().map(|s| s.to_string()).collect();
+        args.insert(0, String::new());
+        let args = CommandLineArgs::from_iter_safe(&args);
+        match args {
+            Ok(args) => match args.action {
+                Load { path } => {
+                    println!("Loading module {}", path);
+                }
+                Unload { module_name } => {
+                    println!("Unloading module {}", module_name);
+                }
+                List => {
+                    println!("Listing modules");
+                }
+                Listfn { module_name } => {
+                    println!("Listing functions in module {}", module_name);
+                }
+                Call {
+                    module_name,
+                    function_name,
+                    args,
+                } => {
+                    println!(
+                        "Calling function {} in module {} with args {:?}",
+                        function_name, module_name, args
+                    );
+                }
+                Exit => {
+                    println!("Exiting");
+                    break;
+                }
+            },
+            Err(err) => {
+                println!("{}", err);
+            }
+        }
+    }
 }
