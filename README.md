cc6194a7478bdb1f2ff415e1de858a09976a72ef
ERUIHNIYHBKBNF <839203521@qq.com>
2022-08-11 11:58:39 +0800

    refactor: wasm

--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 ```bash
 # build wasms for ervery module in the `wasm` directory and move them to the root directory
 # example:
-cd wasm/module1
+cd wasm/module_A
 cargo build --target wasm32-wasi --release
 
-cp target/wasm32-wasi/release/module1.wasm ../../
+cp target/wasm32-wasi/release/module_A.wasm ../../
 
 # build and run the Cli
 cd ../../src
@@ -17,18 +17,4 @@ cargo run
 
 ## Add new wasm modules
 
-Just follow what was done in module1.
-
-Write a wit file in the `wits` directory, and create a new cargo project in the `wasm` directory.
-
-Remember to keep **All** the module name the same (for example `module1` and `Module1` below).
-
-```Rust
-wit_bindgen_rust::export!("../wits/module1.wit");
-
-struct Module1;
-
-impl module1::Module1 for Module1 {
-    // ...
-}
-```
+Just follow what was done in module_A.
---
+++ wasm/module_B/src/main.rs
@@ -0,0 +1,42 @@
+wit_bindgen_rust::import!("../../imports.wit");
+wit_bindgen_rust::export!("../../exports.wit");
+
+struct Exports;
+
+impl exports::Exports for Exports {
+
+    fn proxy(name: String, param: String) -> String{
+        match name.as_str() {
+            "modulef1" => {
+                return modulef1(param);
+            }
+            "modulef2" => {
+                return modulef2(param);
+            }
+            "modulef2" => {
+                return modulef2(param);
+            }
+            _ => return "{}".into(),
+    }
+    }
+
+}
+
+fn modulef1(s: String) -> String{
+    println!("module b, message: {}", s);
+    imports::proxy("f1", "implemented in host");
+    "modulef1".into()
+}
+
+fn modulef2(s: String) -> String {
+    "sdf".into()
+}
+
+fn modulef3(s: String) -> String {
+    println!("implemeted in module");
+    "modulef3".into()
+}
+
+fn main() {
+    println!("from module main");
+}
