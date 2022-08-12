c09f959f51772dd305f8cc5fe6a01cf95a7dd8c7
ERUIHNIYHBKBNF <839203521@qq.com>
2022-08-12 15:26:29 +0800

    refactor: test case

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod cli;
 use cli::{Action::*, CommandLineArgs};
-use core::time;
 use std::io;
 use structopt::StructOpt;
 mod load_module;
@@ -18,17 +17,15 @@ fn cli() {
             Ok(args) => match args.action {
                 Load { path, name } => {
                     println!("Loading module from {}", path);
-                    load_module_by_path(&path, &name).unwrap();
-
+                    load_module_by_path(&path, &name);
                 }
                 Unload { module_name } => {
                     println!("Unloading module {}", module_name);
-                    unload_module_by_name(module_name).unwrap();
-
+                    unload_module_by_name(module_name);
                 }
                 List => {
                     println!("Loaded modules list:");
-                    show_module_list().unwrap();
+                    show_module_list();
                 }
                 Call {
                     module_name,
@@ -39,8 +36,8 @@ fn cli() {
                         "Calling function {} in module {} with param {}",
                         function_name, module_name, param
                     );
-                    let param:serde_json::Value = serde_json::from_str(&param.as_str()).unwrap();
-                    call_module_func(&module_name, &function_name, &param).unwrap();
+                    let param: serde_json::Value = serde_json::from_str(&param.as_str()).unwrap();
+                    call_module_func(&module_name, &function_name, &param);
                 }
                 Exit => {
                     println!("Exiting");
@@ -54,30 +51,41 @@ fn cli() {
     }
 }
 
-fn test() {
-    loop {
-        let dur = time::Duration::from_millis(3000);
-        thread::sleep(dur);
-        
-        let param:serde_json::Value = serde_json::from_str("{\"message\":\"hello_from_cli\"}").unwrap();
-        println!("call from test thread");
-        call_module_func("A", "modulef1", &param);
-    }
-}
-
 fn main() {
     load_module::initialize().unwrap();
     let mut children = vec![];
 
-    children.push(
-        thread::spawn(move || {
-            cli();
-        }));
-    
-    // children.push(
-    //     thread::spawn(move || {
-    //         test();
-    //     }));
+    children.push(thread::spawn(move || {
+        cli();
+    }));
+
+    for child in children {
+        let _ = child.join();
+    }
+}
+
+#[test]
+fn test() {
+    use std::time::Duration;
+
+    load_module::initialize().unwrap();
+    let mut children = vec![];
+
+    children.push(thread::spawn(move || {
+        cli();
+    }));
+
+    children.push(thread::spawn(move || {
+        load_module_by_path(&String::from("module_A.wasm"), &String::from("A")).unwrap();
+        loop {
+            thread::sleep(Duration::from_millis(3000));
+
+            let param: serde_json::Value =
+                serde_json::from_str("{\"message\":\"hello_from_cli\"}").unwrap();
+            println!("call from test thread");
+            call_module_func("A", "modulef1", &param);
+        }
+    }));
 
     for child in children {
         let _ = child.join();
